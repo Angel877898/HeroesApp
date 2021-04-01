43911967c2700b5c1a9fd424ff47e0483bc941cb
Angel877898 <angurie_2008@hotmail.com>
2021-04-01 15:16:13 -0600

    added* login and logout

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
+
 
 
 export const LoginScreen = ({ history }) => {
-
+    const {dispatch} = useContext(AuthContext)
+    
     const handleLogin = () => {
         // history.push('/') //manda a la url pero deja hisotrial para volver atras
-        history.replace('/') // manda a la url pero no deja historial para hacer back
+        // history.replace('/') // manda a la url pero no deja historial para hacer back
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Angel'
+            }
+        })
+        history.replace('/')
     }
 
     return (
